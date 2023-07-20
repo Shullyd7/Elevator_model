b32a31a8fafaea86258492fe57e2cd00cf08446c
Shulammite Odde <shullyd7@gmail.com>
2023-07-20 20:53:56 +0100

    Created models and views

--- elevators/models.py
+++ elevators/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Elevator(models.Model):
+    floor = models.PositiveIntegerField(default=1)
+    direction = models.CharField(max_length=5, choices=[('UP', 'Up'), ('DOWN', 'Down'), ('STOP', 'Stop')], default='STOP')
+    is_moving = models.BooleanField(default=False)
+    is_operational = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Elevator {self.id}"
